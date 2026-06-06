4007de52fbb97bb7f42e6c8ac095ded60108f63c
T <not@vaults.ca>
2026-06-06 12:28:56 -0400

    chore: configure deploy for PWA to GitHub Pages
    - publicPath: / -> /randomly-die for GH Pages deployment
    - deploy script: dist/spa -> dist/pwa

--- quasar.config.js
+++ quasar.config.js
@@ -56,7 +56,7 @@ export default defineConfig(function (/* ctx */) {
 
       // rebuildCache: true, // rebuilds Vite/linter/etc cache on startup
 
-      publicPath: '/',
+      publicPath: '/randomly-die',
       // analyze: true,
       // env: {},
       // rawDefine: {}
